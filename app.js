8293f073673c041cbef611eaf3675192ca5b3617
jatinbharadwaj <jitinrcks@gmail.com>
2020-09-02 02:42:15 +0530

    Authentication

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 const express = require('express')
 const http = require('http')
-
 const app = express()
 const socketio = require('socket.io')
 
 const server = http.createServer(app)
 const io = socketio(server)
 
+let users = {
+    jatin:'jat123'
+}
+let socketMap = {}
+
 io.on('connection',(socket)=>{
     console.log('connected with socket id ',socket.id)
 
-    socket.on('msg_send',(data)=>{
+    function login(s,u)
+    {
+        s.join(u)
+        s.emit('logged_in',u)
+        socketMap[s.id] = u
+        console.log(socketMap)
+    }
 
+    socket.on('login',(data)=>{
+        if(users[data.username]){
+            if(users[data.username] == data.password)
+              { login(socket,data.username)}
+            else    
+               { socket.emit('login_failed')}
+        }
+        else{
+            users[data.username] = data.password
+            login(socket,data.username)
+        }
+    })
+
+    socket.on('msg_send',(data)=>{
+        data.from = socketMap[socket.id]
+        if(data.to){
+            io.to(data.to).emit('msg_rcvd',data)
+        }
+        else{
+            socket.broadcast.emit('msg_rcvd',data)
+        }
         console.log('recieved',data.msg)
 
-        io.emit('msg_rcvd',data)
+        // io.emit('msg_rcvd',data)
 
     })
 
--- public/script.js
+++ public/script.js
@@ -3,14 +3,49 @@ let socket = io()
 let btnSend = $('#btnSend')
 let inpMsg = $('#inpMsg')
 let ulMsgList = $('#ulMsgList')
+let user = $('#User')
+let loginBox = $('#loginBox')
+let chatBox = $('#chatBox')
+let btnStart = $('#btnStart')
+let inpUsername = $('#inpUsername')
+let inpPassword = $('#inpPassword')
+let inpToUser = $('#inpToUser')
+
+loginBox.show()
+chatBox.hide()
+
+btnStart.click(()=>{
+    console.log('btnStart')
+    socket.emit('login',{
+        username:inpUsername.val(),
+        password:inpPassword.val()
+    })
+})
+
+socket.on('logged_in',(data)=>{
+    loginBox.hide()
+    chatBox.show()
+    
+    let useritem = $('<li>',
+    {
+        'class':'list-group-item col-4',
+        text:`Hello ${inpUsername.val()} !!`
+    })
+    user.append(useritem)
+})
+
+socket.on('login_failed',()=>{
+    window.alert('Username or Password Incorrect!')
+})
+
 
 btnSend.click(()=>{
-    let x = inpMsg.val()
-    console.log('here')
     socket.emit('msg_send',{
-        msg:inpMsg.val() 
+        to:inpToUser.val(),
+        msg:inpMsg.val()
     })
-    inpMsg.val(' ') 
+    console.log(inpMsg.val())
+    inpMsg.val('')
 })
 
 socket.on('msg_rcvd',(data)=>{
@@ -18,7 +53,7 @@ socket.on('msg_rcvd',(data)=>{
     let chatitem = $('<li>',
     {
         'class':'list-group-item',
-        text:data.msg
+        text:`From (${data.from}) : ${data.msg}`
     })
     ulMsgList.append(chatitem)
 })
